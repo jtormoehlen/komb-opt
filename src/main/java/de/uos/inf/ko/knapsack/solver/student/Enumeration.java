a41e79e3ac4b8ca0480787d209133b089f2908cd
Johannes Tormöhlen <jtormoehlen@uos.de>
2019-11-06 18:36:36 +0100

    comments added

--- src/main/java/de/uos/inf/ko/knapsack/solver/student/Enumeration.java
+++ src/main/java/de/uos/inf/ko/knapsack/solver/student/Enumeration.java
@@ -7,10 +7,19 @@ import de.uos.inf.ko.knapsack.SolverInterface;
 /**
  * A full enumeration algorithm for the binary knapsack problem.
  * 
- * @author
+ * @author jtormoehlen, luhaupt
  */
 public class Enumeration implements SolverInterface<Solution> {
 
+  /**
+   * generate all binary numbers from 0 to 2^n
+   * and apply each to a knapsack instance;
+   * pick the optimal solution such that:
+   * sum(weight_i) <= W for all i=1,...,n
+   * and value is max
+   * @param instance The given knapsack instance
+   * @return optimal solution
+   */
   @Override
   public Solution solve(Instance instance) {
 
@@ -19,23 +28,36 @@ public class Enumeration implements SolverInterface<Solution> {
     Solution solution = new Solution(instance);
     Solution optimal = solution;
 
+    //convert all integers from 0 to 2^n...
     for (int i = 0; i < Math.pow(2, n); i++) {
+
+      // ...to binary numbers
       String binaryString = Integer.toBinaryString(i);
 
       solution = binaryStringToSolution(binaryString, instance);
 
       if (solution.isFeasible()) {
+
+        //update optimal solution if possible
         if ((solution.getValue() > optimal.getValue())) {
           optimal = binaryStringToSolution(binaryString, instance);
         }
       }
     }
 
+    //print weight and value of optimal solution
     System.out.println("W=" + optimal.getWeight() + "|" + "P=" + optimal.getValue() + "\n");
 
     return optimal;
   }
 
+  /**
+   * apply a binary number (0,1) represented as string
+   * to a knapsack instance
+   * @param binaryString binary number as string
+   * @param instance given knapsack instance
+   * @return solution with (0,1) quantities for each item
+   */
   private static Solution binaryStringToSolution(String binaryString, Instance instance) {
     Solution solution = new Solution(instance);
 
@@ -50,6 +72,14 @@ public class Enumeration implements SolverInterface<Solution> {
     return solution;
   }
 
+  /**
+   * for test purpose, please ignore
+   * @param w
+   * @param v
+   * @param n
+   * @param W
+   * @return
+   */
   private static int knapsackRec(int[] w, int[] v, int n, int W) {
     if (n <= 0) {
       return 0;
