069bcae53def20423303eb6a37d85f91bf25384b
Johannes Tormöhlen <jtormoehlen@uos.de>
2019-11-26 12:35:06 +0100

    CSPSolver easy implemented

--- src/main/java/de/uos/inf/ko/skyscrapers/solver/student/CSPSolver.java
+++ src/main/java/de/uos/inf/ko/skyscrapers/solver/student/CSPSolver.java
@@ -23,18 +23,17 @@ public class CSPSolver {
     }
 
     int n = instance.getGamefieldSize();
-    int[] west = instance.getWest();
 
     // 1. create model
     Model m = new Model("skyscrapers " + n);
 
     // 2. create variables
     IntVar[][] x = m.intVarMatrix("x", n, n, 1, n);
-//    IntVar vN[][] = m.boolVarMatrix("vN", n, n);
-//    IntVar vO[][] = m.boolVarMatrix("vO", n, n);
-//    IntVar vS[][] = m.boolVarMatrix("vS", n, n);
+    IntVar[][] xT = m.intVarMatrix("xT", n, n, 1, n);
+    BoolVar[][] vN = m.boolVarMatrix("vN", n, n);
+    BoolVar[][] vE = m.boolVarMatrix("vE", n, n);
+    BoolVar[][] vS = m.boolVarMatrix("vS", n, n);
     BoolVar[][] vW = m.boolVarMatrix("vW", n, n);
-    IntVar[] lines = m.intVarArray("lines", n, 1, n);
 
 
     // 3. add constraints
@@ -50,23 +49,49 @@ public class CSPSolver {
         if (instance.getGamefield()[i][j] > 0) {
           m.arithm(x[i][j],"=", instance.getGamefield()[i][j]).post();
         }
+
+        m.arithm(xT[j][i], "=", x[i][j]).post();
       }
     }
 
     for (int i = 0; i < n; i++) {
       m.sum(vW[i], "=", instance.getWest()[i]).post();
+      m.sum(vE[i], "=", instance.getEast()[i]).post();
+      m.sum(vN[i], "=", instance.getNorth()[i]).post();
+      m.sum(vS[i], "=", instance.getSouth()[i]).post();
       m.arithm(vW[i][0], "=", 1).post();
+      m.arithm(vE[i][n - 1], "=", 1).post();
+      m.arithm(vN[i][0], "=", 1).post();
+      m.arithm(vS[i][n - 1], "=", 1).post();
     }
 
     for (int i = 0; i < n; i++) {
       for (int j = 1; j < n; j++) {
-        Constraint[] constraints = new Constraint[j];
+        Constraint[] westConstraints = new Constraint[j];
+        Constraint[] northConstraints = new Constraint[j];
 
         for (int h = 0; h < j; h++) {
-          constraints[h] = m.arithm(x[i][h], "<", x[i][j]);
+          westConstraints[h] = m.arithm(x[i][h], "<", x[i][j]);
+          northConstraints[h] = m.arithm(xT[i][h], "<", xT[i][j]);
         }
 
-        m.ifOnlyIf(m.arithm(vW[i][j], "=", 1), m.and(constraints));
+        m.ifOnlyIf(m.arithm(vW[i][j], "=", 1), m.and(westConstraints));
+        m.ifOnlyIf(m.arithm(vN[i][j], "=", 1), m.and(northConstraints));
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - 1; j++) {
+        Constraint[] eastConstraints = new Constraint[n - (j + 1)];
+        Constraint[] southConstraints = new Constraint[n - (j + 1)];
+
+        for (int h = j + 1; h < n; h++) {
+          eastConstraints[h - (j + 1)] = m.arithm(x[i][h], "<", x[i][j]);
+          southConstraints[h - (j + 1)] = m.arithm(xT[i][h], "<", xT[i][j]);
+        }
+
+        m.ifOnlyIf(m.arithm(vE[i][j], "=", 1), m.and(eastConstraints));
+        m.ifOnlyIf(m.arithm(vS[i][j], "=", 1), m.and(southConstraints));
       }
     }
 
@@ -74,7 +99,6 @@ public class CSPSolver {
     Solver s = m.getSolver();
     List<Solution> solutions = new ArrayList<>();
     solutions.add(s.findSolution());
-    printVarMat(vW, solutions.get(0));
 
     // 5. print solutions
     int size = instance.getGamefieldSize();
@@ -97,6 +121,36 @@ public class CSPSolver {
     }
 
     return instance;
+  }
+
+  private static void checkArray(Constraint[] array) {
+    String res = "";
+
+    for (int index = 0; index < array.length; index++) {
+      try {
+        res += array[index].toString() + "\n";
+      } catch (NullPointerException e) {
+        System.out.println(index + "/" + array.length);
+      }
+    }
+
+    System.out.println(res);
+  }
+
+  /**
+   * for test purpose
+   * @param list of constraints
+   * @return array of constraints
+   */
+  private static Constraint[] listToArray(List<Constraint> list) {
+    Constraint[] array = new Constraint[list.size()];
+
+    int index = 0;
+    for (Constraint constraint : list) {
+      array[index] = constraint;
+    }
+
+    return array;
   }
 
   private static void printVarMat(IntVar[][] varMat, Solution s) {
